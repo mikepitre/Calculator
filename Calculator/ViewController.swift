e7067e4a8a251ae52e7dd42fbb16c85a7fcebd73
mikepitre <michaelepitre@gmail.com>
2015-10-21 13:00:35 -0500

    sounded added to btn press on numbers 1-9

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,14 +14,34 @@ class ViewController: UIViewController {
     //Outlets
     @IBOutlet weak var outputLbl: UILabel!
     
-    //Actions
-    
-    
+    //btn pressed sound
+    var btnSound: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //load path for audio file
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        //pass path into sound url
+        let soundUrl = NSURL.fileURLWithPath(path!)
+        
+        //try to load sound from url, if can't print error to console
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
     }
+    
+    //Actions
+    @IBAction func numberPressed(sender: AnyObject) {
+        btnSound.play()
+    }
+    
 
 
 }
