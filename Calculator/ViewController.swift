23b24ce38414f395ad207d8462c314df7f7d5430
mikepitre <michaelepitre@gmail.com>
2015-10-21 12:11:53 -0500

    added sound file to project

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    //Actions
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
